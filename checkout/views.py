8858b9fa158bba2c9c7b52f222cc4cddf6b5270d
Hgoatly <hgoatly@hotmail.com>
2021-03-25 22:37:09 +0000

    Add checkout views and template.

--- checkout/views.py
+++ checkout/views.py
@@ -7,7 +7,7 @@ from .forms import OrderForm
 def checkout(request):
     bag = request.session.get('bag', {})
     if not bag:
-        messages.effof(request, "There's nothing in your bag at the moment")
+        messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
 
     order_form = OrderForm
