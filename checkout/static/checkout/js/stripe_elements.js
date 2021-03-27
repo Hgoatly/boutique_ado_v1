5f12c5439deb5b5da6fa8f15b269729cab4ac2d8
Hgoatly <hgoatly@hotmail.com>
2021-03-27 07:44:10 +0000

    Add basic chekout functionality.

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -7,9 +7,9 @@
     https://stripe.com/docs/stripe-js
 */
 
-var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
-var client_secret = $('#id_client_secret').text().slice(1, -1);
-var stripe = Stripe(stripe_public_key);
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
 /* This copied from the walkthrough source code as I couldn't find it on the stripe docs. */
 var style = {
@@ -46,3 +46,34 @@ card.addEventListener('change', function (event) {
     }
 })
 
+// Handle form submit. Code from stripe docs
+form.addEventListener('submit', function(ev) {
+  ev.preventDefault();
+    card.update({'disabled': true})
+    $('#submit-button').attr('disabled', true);
+  // If the client secret was rendered server-side as a data-secret attribute
+  // on the <form> element, you can retrieve it here by calling `form.dataset.secret`
+  stripe.confirmCardPayment(clientSecret, {
+    payment_method: {
+      card: card,
+      
+    }
+  }).then(function(result) {
+    if (result.error) {
+        var errorDiv = document.getElementById('card-errors');
+        var html = `
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+    <span>${event.error.message}</span>`;
+    
+    $(errorDiv).html(html);
+     card.update({'disabled': false})
+    $('#submit-button').attr('disabled', false);
+    } else {
+      if (result.paymentIntent.status === 'succeeded') {
+        form.submit();
+      }
+    }
+  });
+});
