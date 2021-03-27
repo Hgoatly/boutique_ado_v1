1182cf2b004898b0631adc96c42a579a52704223
Hgoatly <hgoatly@hotmail.com>
2021-03-27 23:36:30 +0000

    Add loading overlay.

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -11,7 +11,6 @@ var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
 var clientSecret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
-/* This copied from the walkthrough source code as I couldn't find it on the stripe docs. */
 var style = {
     base: {
         color: '#000',
@@ -30,7 +29,7 @@ var style = {
 var card = elements.create('card', {style: style});
 card.mount('#card-element');
 
-// Handle realtime validation errors on the card element.
+// Handle realtime validation errors on the card element
 card.addEventListener('change', function (event) {
     var errorDiv = document.getElementById('card-errors');
     if (event.error) {
@@ -38,42 +37,44 @@ card.addEventListener('change', function (event) {
             <span class="icon" role="alert">
                 <i class="fas fa-times"></i>
             </span>
-    <span>${event.error.message}</span>`;
-    
-    $(errorDiv).html(html);
+            <span>${event.error.message}</span>
+        `;
+        $(errorDiv).html(html);
     } else {
         errorDiv.textContent = '';
     }
-})
+});
 
-// Handle form submit. Code from stripe docs
+// Handle form submit
+var form = document.getElementById('payment-form');
+
 form.addEventListener('submit', function(ev) {
-  ev.preventDefault();
-    card.update({'disabled': true})
+    ev.preventDefault();
+    card.update({ 'disabled': true});
     $('#submit-button').attr('disabled', true);
-  // If the client secret was rendered server-side as a data-secret attribute
-  // on the <form> element, you can retrieve it here by calling `form.dataset.secret`
-  stripe.confirmCardPayment(clientSecret, {
-    payment_method: {
-      card: card,
-      
-    }
-  }).then(function(result) {
-    if (result.error) {
-        var errorDiv = document.getElementById('card-errors');
-        var html = `
-            <span class="icon" role="alert">
+    $('#payment-form').fadeToggle(100);
+    $('#loading-overlay').fadeToggle(100);
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function(result) {
+        if (result.error) {
+            var errorDiv = document.getElementById('card-errors');
+            var html = `
+                <span class="icon" role="alert">
                 <i class="fas fa-times"></i>
-            </span>
-    <span>${event.error.message}</span>`;
-    
-    $(errorDiv).html(html);
-     card.update({'disabled': false})
-    $('#submit-button').attr('disabled', false);
-    } else {
-      if (result.paymentIntent.status === 'succeeded') {
-        form.submit();
-      }
-    }
-  });
+                </span>
+                <span>${result.error.message}</span>`;
+            $(errorDiv).html(html);
+            $('#payment-form').fadeToggle(100);
+            $('#loading-overlay').fadeToggle(100);
+            card.update({ 'disabled': false});
+            $('#submit-button').attr('disabled', false);
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
+            }
+        }
+    });
 });
