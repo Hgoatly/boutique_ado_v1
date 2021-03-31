c8865ad035c8777faf9078d0dd4277fbb7af7690
Hgoatly <hgoatly@hotmail.com>
2021-03-31 10:14:13 +0000

    Product admin: Finish add product functionality.

--- products/views.py
+++ products/views.py
@@ -28,7 +28,7 @@ def all_products(request):
             if sortkey == 'category':
                 sortkey = 'category__name'
             if 'direction' in request.GET:
-                direction= request.GET['direction']
+                direction = request.GET['direction']
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)        
@@ -74,7 +74,17 @@ def product_detail(request, product_id):
 
 def add_product(request):
     """ Add a product to the store """
-    form = ProductForm()
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+    else:
+        form = ProductForm()
+
     template = 'products/add_product.html'
     context = {
         'form': form,
